e55b4b26d738b629534d0221e5477786a84eb350
Waleed Khalid <waleed@codexsquared.com>
2022-02-15 15:57:26 +0500

    Update code Waleed

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
-import ReactDOM from 'react-dom'
+import React,{ Component }  from 'react';
+ 
+class App extends Component {
+ 
+  render() {
+    return (    
+       <div>
+         <h1>Hello, Learner.Welcome to GeeksforGeeks.</h1>
+       </div>
+ 
+    );
+  }
+}
+    
+export default App;
