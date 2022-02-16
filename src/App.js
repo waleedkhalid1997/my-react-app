e3b84901173be2ef65d5cb943ce034d2809ee119
Waleed Khalid <waleed@codexsquared.com>
2022-02-16 16:50:44 +0500

    Update code Waleed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React,{ Component }  from 'react';
- 
-class App extends Component {
- 
-  render() {
-    return (    
-       <div>
-         <h1>Hello, Learner.Welcome to GeeksforGeeks.</h1>
-       </div>
- 
-    );
-  }
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-    
+
 export default App;
